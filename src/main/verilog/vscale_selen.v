99dc271d77fc2a0425df63a3b23eda9b28b61066
Grigoriy Zhikharev <zhiharev@tecon.ru>
2016-03-31 08:16:14 +0300

    Fixed selen core interface for writes (delayed data)

--- src/main/verilog/vscale_selen.v
+++ src/main/verilog/vscale_selen.v
@@ -38,8 +38,12 @@ module core_top
   wire [`XPR_LEN-1:0]        dmem_rdata;
   wire                       dmem_badmem_e;
 
-  wire d_req_cop_wr;
-  wire d_req_cop_nc;
+  wire                       d_req_cop_wr;
+  wire                       d_req_cop_nc;
+
+  reg                        dmem_en_delayed;
+  reg [`XPR_LEN-1:0]         dmem_addr_delayed;
+  reg [`MEM_TYPE_WIDTH-1:0]  dmem_size_delayed;
 
   vscale_pipeline vscale
   (
@@ -68,22 +72,44 @@ module core_top
     .htif_pcr_resp_data   ()
   );
 
+  always @(posedge clk or negedge rst_n) begin
+    if(~rst_n) begin
+      dmem_addr_delayed <= 0;
+      dmem_size_delayed <= 0;
+    end else begin
+      if(dmem_wen) begin
+        dmem_addr_delayed <= dmem_addr;
+        dmem_size_delayed <= dmem_size;
+      end
+    end
+  end
+
+  always @(posedge clk or negedge rst_n) begin
+    if(~rst_n) begin
+      dmem_en_delayed   <= 1'b0;
+    end
+    else begin
+      if(dmem_en_delayed) dmem_en_delayed <= ~d_req_ack;
+      else dmem_en_delayed <= dmem_en & dmem_wen;
+    end
+  end
+
   assign i_req_val      = 1'b1;
   assign i_req_addr     = imem_addr;
   assign imem_wait      = ~i_req_ack;
   assign imem_rdata     = i_ack_rdata;
   assign imem_badmem_e  = 1'b0;
 
-  assign d_req_val      = dmem_en;
-  assign d_req_addr     = dmem_addr;
+  assign d_req_val      = dmem_en_delayed | (dmem_en & ~dmem_wen);
+  assign d_req_addr     = (dmem_en_delayed) ? dmem_addr_delayed : dmem_addr;
   assign d_req_cop      = {1'b0, d_req_cop_nc, d_req_cop_wr};
   assign d_req_wdata    = dmem_wdata_delayed;
-  assign d_req_size     = dmem_size;
+  assign d_req_size     = (dmem_en_delayed) ? dmem_size_delayed : dmem_size;
   assign dmem_wait      = ~d_req_ack;
   assign dmem_rdata     = d_ack_rdata;
   assign dmem_badmem_e  = 1'b0;
 
-  assign d_req_cop_wr   = dmem_wen;
+  assign d_req_cop_wr   = dmem_en_delayed;
   assign d_req_cop_nc   = (dmem_addr & ~({32{1'b0}} | NC_OFFSET)) == NC_BASE_ADDR;
 
 endmodule
